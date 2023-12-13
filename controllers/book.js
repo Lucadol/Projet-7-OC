da70675c500dffb78ad949a5be46a688a775fb70
Lucas Leib <lucas.leib@gmail.com>
2023-12-13 09:48:10 +0100

    feat : add rating

--- controllers/book.js
+++ controllers/book.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 exports.createBook = (req, res, next) => {
    const bookObject = JSON.parse(req.body.book);
    delete bookObject._id;
-   delete bookObject._userId;
+   delete bookObject.userId;
    const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
@@ -63,12 +63,84 @@ exports.getAllBooks = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
 };
 
-// exports.bestrating = (req, res, next) => {
-//   Book.find().sort({ averageRating: -1 }).limit(3)
-//     .then((bestRatedBooks) => {
-//       res.status(200).json(bestRatedBooks);
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ error: 'Erreur lors de la récupération des livres les mieux notés.' });
-//     });
-// };
+exports.getBestRating = (req, res, next) => {
+  // Récupère tous les livres de la base de données
+  Book.find()
+      .then((books) => {
+          // Vérifie s'il n'y a aucun livre dans la base de données
+          if (!books.length) {
+              return res.status(404).json({ message: "Aucun livre dans la base de données." });
+          }
+
+          // Trie les livres par note moyenne et date d'ajout
+          const sortedBooks = books.sort((a, b) => {
+              if (b.averageRating !== a.averageRating) {
+                  return b.averageRating - a.averageRating;
+              }
+              // Si deux livres ont la même note moyenne, on privilégie le livre ajouté le plus récemment
+              return books.indexOf(b) - books.indexOf(a);
+          });
+
+          // Sélectionne les trois meilleurs livres
+          const bestBooks = sortedBooks.slice(0, 3);
+
+          return res.status(200).json(bestBooks);
+      })
+      .catch((err) => {
+          res.status(err.status || 500).json({ error: err.message || "Une erreur inattendue s'est produite." });
+      });
+}
+
+
+exports.rateBook = (req, res) => {
+  // Vérifie si le corps de la requête est vide
+  const isBodyEmpty = Object.keys(req.body).length === 0 ? true : false;
+
+  if (isBodyEmpty) {
+      return res.status(400).json({ message: "Le corps de la requête est vide." });
+  }
+
+  let newRating = { ...req.body };
+
+  // Vérifie que la notation est dans la plage autorisée
+  if (newRating.rating < 0 || newRating.rating > 5) {
+      return res.status(400).json({ message: "La notation doit être entre 0 et 5." });
+  }
+
+  Book.findOne({ _id: req.params.id })
+      .then((bookToRate) => {
+          if (!bookToRate) {
+              return res.status(404).json({ message: "Le livre n'existe pas." });
+          }
+
+          // Actualise le userId avec celui du token JWT pour s'assurer de l'identité de l'utilisateur
+          newRating.userId = req.auth.userId;
+          // Remplace la clé rating (envoyée par la requête) par la clé grade (attendue par le modèle Book)
+          newRating.grade = newRating.rating;
+          delete newRating.rating;
+
+          // Vérifie si l'utilisateur a déjà déposé une note
+          const hasUserAlreadyRated = bookToRate.ratings.some((item) => item.userId === newRating.userId);
+
+          if (hasUserAlreadyRated) {
+              return res.status(400).json({ message: "Vous avez déjà noté ce livre." });
+          }
+
+          // Calcul de la nouvelle note moyenne
+          bookToRate.ratings.push(newRating);
+          const sum = bookToRate.ratings.reduce((total, item) => total + item.grade, 0);
+          bookToRate.averageRating = sum / bookToRate.ratings.length;
+
+          return bookToRate.save();
+      })
+      .then((updatedBook) => {
+          return res.status(200).json(updatedBook);
+      })
+      .catch((err) => {
+          if (err.message.includes("MONGODB_OBJECTID_ERROR")) {
+              res.status(404).json({ message: "Le livre n'existe pas." });
+          } else {
+              res.status(err.status || 500).json({ error: err.message || "Une erreur inattendue s'est produite." });
+          }
+      });
+}
--- models/book.js
+++ models/book.js
@@ -10,7 +10,7 @@ const bookSchema = mongoose.Schema({
   ratings: [
       {
           userId: { type: String },
-          grading: { type: Number }
+          grade: { type: Number }
       }
   ],
   averageRating: { type: Number }
